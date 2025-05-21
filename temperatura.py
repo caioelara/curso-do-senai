6cd6bc5e060e7989bfae0e3a2a4c783ddbf06e73
caioelara <caio.condetavares@gmail.com>
2025-05-21 15:52:20 -0300

    3

---
+++ temperatura.py
@@ -0,0 +1,9 @@
+tempo=float(input('qual e a temperatura? '))
+if tempo>=30:
+    print('ta quente!!')
+elif tempo>=20:
+    print('clima agradavel')
+elif tempo>=10:
+    print('ta frio')
+else:
+    print('ta muito frio')
