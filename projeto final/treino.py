1422c145a078f5a792edff3cfc26372b7ba0f47a
caioelara <caio.condetavares@gmail.com>
2025-07-01 15:50:38 -0300

    Add files via upload

---
+++ projeto final/treino.py
@@ -0,0 +1,46 @@
+treino={
+     'treino':[' supino reto com barra 4x12'],
+     'data':['30/06']
+}
+
+
+
+def menu():
+    print('//--//  //--//  TREINO  //--//  //--//')
+    print('(1)-Mostrar Treino')
+    print('(2)-Adicionar Treino')
+    print('(3)-Remover Treino')
+    print('(4)-Sair')
+    print('//--//  //--//  //--//  //--//  //--//')
+    opcao=int(input('digite um numero: '))
+
+def mostrar_treino{
+    for i in range(len(treino['treino'])):
+        print(f'{i+1}. {treino['treino'][i]}- Data: {treino['data'][i]}')
+}
+   
+    
+#    if opcao==1:
+        
+
+ #   elif opcao==2:
+  #      ex_novo=input('adicione um exercicio:')
+   #     data_novo=input('adicione uma data:')
+    #    return ex_novo,data_novo
+        
+    #elif opcao==3:
+     #   print('vc escolheu opção 3')
+    #elif opcao==4:
+    #    print('vc escolheu opção 4')
+    #else:
+    #    print('Opção errada =(')
+
+
+
+#menu()
+
+
+
+
+
+
